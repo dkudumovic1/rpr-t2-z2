154216a1b59b5e4e5701d8b569bb5d34ea2ac6da
dkudumovic1 <dkudumovic1@etf.unsa.ba>
2019-10-23 16:39:24 +0200

    Napisani konstruktori i metoda isNull

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,37 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double x;
+    private double y;
+    private boolean prvaGranica;
+    private boolean drugaGranica;
 
 
+    public static Interval intersect(Interval i, Interval i2) {
+    }
+
+    public Interval(double x,double y,boolean prva,boolean druga) {
+        if(x>y) throw IllegalArgumentException;
+        this.x = x;
+        this.y = y;
+        prvaGranica=prva;
+        drugaGranica=druga;
+
+    }
+
+    public Interval() {
+        this.x=0;
+        this.y=0;
+        prvaGranica=false;
+        drugaGranica=false;
+    }
+
+    public boolean isIn(double v) {
+
+    }
+
+    public boolean isNull() {
+        if(x!=0 || y!=0 || prvaGranica || drugaGranica) return false;
+        return true;
+    }
 }
