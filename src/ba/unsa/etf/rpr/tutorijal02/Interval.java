b4e3a4050ab274aae56f03d16fb609761b4fe036
dkudumovic1 <dkudumovic1@etf.unsa.ba>
2019-10-24 23:01:56 +0200

    .

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -8,10 +8,13 @@ public class Interval {
 
 
     public static Interval intersect(Interval i, Interval i2) {
+    return i;
     }
 
-    public Interval(double x,double y,boolean prva,boolean druga) {
-        if(x>y) throw IllegalArgumentException;
+    public Interval(double x,double y,boolean prva,boolean druga) throws IllegalArgumentException {
+        if(x>y) {
+            throw new IllegalArgumentException ("Prva granica veca od druge");
+        }
         this.x = x;
         this.y = y;
         prvaGranica=prva;
@@ -26,12 +29,45 @@ public class Interval {
         drugaGranica=false;
     }
 
-    public boolean isIn(double v) {
-
+    public double getX() {
+        return x;
     }
 
-    public boolean isNull() {
-        if(x!=0 || y!=0 || prvaGranica || drugaGranica) return false;
+    public double getY() {
+        return y;
+    }
+
+    public boolean isPrvaGranica() {
+        return prvaGranica;
+    }
+
+    public boolean isDrugaGranica() {
+        return drugaGranica;
+    }
+
+    public boolean isIn(double v) {
+        if(v<x || v>y) return false;
+        if(!isPrvaGranica() && v==x) return false;
+        if(!isDrugaGranica() && v==y) return false;
         return true;
+     }
+
+    public boolean isNull() {
+        if(x!=0 || y!=0 || isPrvaGranica() || isDrugaGranica()) return false;
+        return true;
+    }
+
+    public Interval intersect(Interval interval) {
+
+        if(x==interval.x && isPrvaGranica() && interval.isPrvaGranica()) {
+            if(y<interval.y ) return this;
+            else if(y>interval.y) {
+                if(interval.isDrugaGranica())5 {
+                    return new Interval(x,y,true,);
+                }
+
+            }
+        }
+        if()
     }
 }
